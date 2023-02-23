ae2b1df1495535415d2590fa25e622cc48efd13e
MuhammadAdil <drcadil388@gmail.com>
2023-02-23 12:59:52 +0500

    login

--- Routes/auth.js
+++ Routes/auth.js
@@ -75,7 +75,7 @@ router.post(
 );
 
 /* This is a route that is used to login the user. */
-router.post(async (req, res) => {
+router.post("/login", async (req, res) => {
   let { email, password } = req.body;
   try {
     let user = await User.findOne({ email });
